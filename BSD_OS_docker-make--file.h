d39625d32a9b0dc27018ba8935bdd2fc54c94678
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-04-23 21:34:13 -0500

    ^D
    Signed-off-by: Z4 <30985576+TheProdigyLeague@users.noreply.github.com>

--- BSD_OS_docker-make--file.h
+++ BSD_OS_docker-make--file.h
@@ -1,9 +1,9 @@
-# $Id$
-# Redirect BSD make to GNU gmake for convenience
-
+$Id$
+$ Redirect BSD make to GNU gmake for convenience
+~
 USE_GNU:
 	@gmake $(.TARGETS)
-
+~
 $(.TARGETS): USE_GNU
-
+~
 .PHONY: USE_GNU
