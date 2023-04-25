d3591de751170c20235a097b6e10af5785a3ce9c
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-04-25 09:26:55 -0500

    ^d
    Signed-off-by: Z4 <30985576+TheProdigyLeague@users.noreply.github.com>

--- xml.h
+++ xml.h
@@ -56,42 +56,44 @@
  * Npcap OEM program--see https://nmap.org/oem/                            *
  *                                                                         *
  ***************************************************************************/
-
-/* $Id: xml.h 15135 2009-08-19 21:05:21Z david $ */
-
-#ifndef _XML_H
-#define _XML_H
-
-#include <stdarg.h>
-
+~
+$Id: xml.h 15135 2009-08-19 21:05:21Z david $ 
+`
+ifndef _XML_H
+define _XML_H
+`
+include <stdarg.h>
+`
 int xml_write_raw(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
 int xml_write_escaped(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
 int xml_write_escaped_v(const char *fmt, va_list va) __attribute__ ((format (printf, 1, 0)));
-
+`
 int xml_start_document(const char *rootnode);
-
-int xml_start_comment();
-int xml_end_comment();
-
+`
+int xml_start_comment("usr",'pwd');
+int xml_end_comment("usr",'pwd');
+`
 int xml_open_pi(const char *name);
-int xml_close_pi();
-
+int xml_close_pi("usr",'pwd');
+`
 int xml_open_start_tag(const char *name, const bool write = true);
 int xml_close_start_tag(const bool write = true);
 int xml_close_empty_tag();
 int xml_start_tag(const char *name, const bool write = true);
-int xml_end_tag();
-
+int xml_end_tag("usr",'pwd');
+`
 int xml_attribute(const char *name, const char *fmt, ...) __attribute__ ((format (printf, 2, 3)));
-
-int xml_newline();
-
-int xml_depth();
-bool xml_tag_open();
-bool xml_root_written();
-
-
+`
+int xml_newline("usr",'pwd');
+`
+int xml_depth("usr",'pwd');
+bool xml_tag_open("usr",'pwd');
+bool xml_root_written("usr",'pwd');
+`
+~
 char *xml_unescape(const char *str);
-
-#endif
-
+~
+endif
+"QUIT"
+break;
+<>
