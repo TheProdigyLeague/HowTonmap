b5a9ba516baabad0405a82cce483f3057fa06a30
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-04-27 10:55:14 -0500

    ^d
    Signed-off-by: Z4 <30985576+TheProdigyLeague@users.noreply.github.com>

--- nse_libssh2.h
+++ nse_libssh2.h
@@ -1,8 +1,9 @@
-#ifndef LIBSSH2
-
-#define LIBSSH2
-#define LIBSSH2LIBNAME "libssh2"
-
-int luaopen_libssh2 (lua_State *L);
-
-#endif
+# ifndef _LIBSSH2
+~
+# define _LIBSSH2
+# define _LIBSSH2LIBNAME "libssh2"
+~
+>>> int luaopen_libssh2 (lua_State *L);
+~
+# endif
+"quit"
