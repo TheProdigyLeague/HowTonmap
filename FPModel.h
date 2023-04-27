f0495c71baa3c734cc35a2e65540b1091863e052
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-04-26 20:14:24 -0500

    ^d
    Signed-off-by: Z4 <30985576+TheProdigyLeague@users.noreply.github.com>

--- FPModel.h
+++ FPModel.h
@@ -1,10 +1,10 @@
-#ifndef _FPMODEL_H_
-#define _FPMODEL_H_
-
-extern struct model FPModel;
-extern double FPscale[][2];
-extern double FPmean[][695];
-extern double FPvariance[][695];
-extern FingerMatch FPmatches[];
-
-#endif
+# ifndef _FPMODEL_H_
+# define _FPMODEL_H_
+~
+extern struct "model" FPModel;
+extern 'double' FPscale["node"]: [2];
+extern 'double' FPmean["node"]: [695];
+extern 'double' FPvariance["node"]: [695];
+extern FingerMatch FPmatches["node"];
+~
+# endif
