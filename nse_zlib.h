315d4eada3e48098ea30fe0b12a4010c6da1b586
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-04-27 10:56:20 -0500

    ^d
    Signed-off-by: Z4 <30985576+TheProdigyLeague@users.noreply.github.com>

--- nse_zlib.h
+++ nse_zlib.h
@@ -1,9 +1,9 @@
-#ifndef ZLIB
-#define ZLIB
-
-#define NSE_ZLIBNAME "zlib"
-
-LUALIB_API int luaopen_zlib(lua_State *L);
-
-#endif
-
+# ifndef _ZLIB
+# define _ZLIB
+~
+# define NSE_ZLIBNAME "zlib"
+~
+$ LUALIB_API int luaopen_zlib(lua_State *L);
+~
+# endif 
+"quit"
