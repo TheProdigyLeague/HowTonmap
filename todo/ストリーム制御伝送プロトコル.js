a5ef0853e4707bc6fe674deb546a1aa28a658f44
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-05-09 12:58:46 -0500

    ストリーム制御伝送プロトコル.js
    .py
    
    Signed-off-by: Z4 <30985576+TheProdigyLeague@users.noreply.github.com>

---
+++ todo/ストリーム制御伝送プロトコル.js
@@ -0,0 +1,33 @@
+#nse.sctp 
+:root
+$Id$ -*-  p  i  x  e  l  -*-
+>>> import bloc:S 
+>>> import libs 
+>>> import mods
+>>> from o investigate nse.SCTP ( as some people reported problems (see this thread:_blank)~);
+break;`
+srcURL="http://seclists.org/nmap-dev/2009/q2/0669.html"
+{`["+","supp"]: for nse.udp ("encapsule","sctp"); \9899/udp /!\ basic wrap ["+"]: for this support @libdnet 
+see this int @Michael_Tuexen(c) for 分光光学,;;break
+~o srcURL="http://tools.ietf.org/html/draft-tuexen-sctp-udp-encaps"
+this "arch32x64x86" *scan.nse 
+// note: How to best differentiate a UDP packet related to a UDP scan from a UDP wrapped SCTP packet?  
+// How to unpack the UDP wrapped SCTP packet in order not to duplicate a lot of code? A good solution will be non-trivial.
+// Verify ICMP response handling for SCTP  
+// ALL ICMP types are handled in an optimal way (esp. destination unreachable: protocol unreachable).
+~o $ rm 9899/sctp from default port List 9899/udp is ["use"]: for __UDP, ("encapsulated"), SCTP__
+➜ 9899/sctp is mis.config
+.c
+.h
+~o スキャン状態の保存 in the itag/itsn
+▶ fields for INIT scans !
+~o burp_suite *sctp chunk for \port \scan & imp > scan.js type if turn-out 
+["1"]: ["!"]: unverified is experiment with undefined chunk types && two magic bits to provoke ERROR responses.
+~o ["+"]: SCTP based srvc probe
+~o ["Ncat"]: imp_SCTP -broker mode.
+~o ["NSE","+"]: SCTP_support.NSE
+$ -diff betwix sctp_stack && imp sctp_baseOS --detect --probe ["base"]: 
+$ for bsdOS <?php detect probe base result?>
+	bsdOS send ASCII "string" KAME-BASD in INIT-ACK_chunk
+~o SCTP-enable scanme.nmap.org in order to make scanme.roe.ch || obsolete
+};break;"quit"
