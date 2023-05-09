5c1f26b8edd543e1cc5b0666b356ada13a2206d7
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-05-09 13:45:09 -0500

    c++ 小鬼缓存。.py
    ^d^re^mod
    
    Signed-off-by: Z4 <30985576+TheProdigyLeague@users.noreply.github.com>

---
+++ todo/小鬼缓存。.py
@@ -0,0 +1,43 @@
+>>> lpeg.nse http://libs.LPeg/include
+>>> script.nse -d @Diman \imp
+['-']: http://seclists.org/nmap-dev/2008/q1/0228.html
+['-']: http://www.keplerproject.org/remdebug/
+['-']: nsock.nse ;; **alloc
+o Dynamically > socket _slot if {"void"};~5.s , < traffic is work
+         this resolve any sort of socket deadlock
+['-']: -deadlockID correct:
+    o ['+']: detect for deadlocks('&&'): print('/thread/');;
+    o ['use']: ^ result @makeFile strat for auto deadlock ~resolution
+['-']: mv Pkt++ Mod .c
+Done!
+['-']: &&  imp.nse _Nsock _Libs
+o mv from .c ➜ ref && **alloc ^ L  u  a
+~
+          if function("end, error! all usr.dat collect...need usr pcalls! clean and free malloc()'d mem");
+          ['use','thread','call']: $nsock_loop ~thread
+o for restore wait threads to run queue
+$ make function('call') in yield {'thread' is ['meta','sploit']: && --Vuln
+o --git rm of ** static + nsock_pool .c++ ['use']: "dynamically_allocated structure" in per-host-group base
+o prepare for Lua 5.2 
+▶ Change to real errors
+$ --Upd book.nse -Imp "Section"
+$ --mod ["+"] bool op -patch
+$ --Upd script.NSE --script "Section" (book) include boolean op
+▶ --ceil for run-levels FIX
+▶ --segfault for thread_sockets && close this thread end ('SOLVE');
+➜ --mod "error in ["FIND"]: Name of **nonexistent fs script.db in non-fatal warning..."
+➜ nsock_connect in unlock-the-sock ["slot"] if connection Fails.
+$ --rm packet.hextobin and packet.bintohex. 
+**Fix scripts that used them
+/!\ instead use bin.(un)pack.
+>>> -- Commit --script-args patch 
+>>> upd Relevant Section in book.py
+>>> --dead_lockID ('correct');
+o release mutexes || if s c  r  i  p  t スクリプトの死
+➜ --script.NSE --Nsock __Socket || **Allocation:
+    o Release socket locks on connection failure or timeout.
+    o Track active sockets in the nsock library and do not rely on garbage collection for reallocation.
+➜  -- HTTP Caching:▶["+"]: enable ["use"]: burp_suite \proxy @@http.lua | test magic http.lua perf/local/cache/proxy
+▶ imp cache in http.lua
+~
+   "quit"{'void'}break;
