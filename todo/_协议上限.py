b8121bdeb0fb31be193c43f13ae0b290fe4e3783
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-05-09 14:24:18 -0500

    henri_协议上限.py
    ^d^re^mod .ext
    
    Signed-off-by: Z4 <30985576+TheProdigyLeague@users.noreply.github.com>

---
+++ todo/_协议上限.py
@@ -0,0 +1,62 @@
+o imp.SSL supp in proxy error mode
+  [-]: native imp relay 
+  	in current code.exe 
+		.h .c
+			Random Attempt
+				<?php this nsock Internally Enable @@SSLify ** plain.txt __TCP(connection) NULL=export ?>
+					imp'd *op 
+						then Trivialize && Clean 
+							for libs from @@SSLify.chan 
+								**proxy_.h
+			catch,
+	return 0,
+{'void'};
+  [-]: if -re -design --nsock SSL.c catch enable <?php establish OpenSSL session && xploit Vuln from raw TCP.pkt++gtk++ 
+  	then, audit SSL/TLS layer.socks
+return 0 ;;
+>>> ~o note: No Drop_v.pending...Writes
+>>> del cor_IOD for nsock -priveleges StandardBSD.Socks flush write "in Close"
+➜ (OTOH anything)_(Which is not 'ack' and has no meaning. Caller can still cancel...)
+:root$antlr>
+o Give IODs their own methods to streamline the code and get rid of all the special cases in nsock_core.c. 
+note: This would also make it easier to hook operations (typically: override the default iod_connect() method to establish a proxy chain).
+["!"]: read api...
+o 剖析 [ pcap.nse ] 代码。它需要清理（肯定）和优化（可能）。
+0auth_proxy.c
+ o Handle socks4a
+  [-] this require 
+  	trigger burp_suite \proxy \code <?php resolve target host Name = 1 ?> 
+		proxy__code is Transparent .h* 
+			connect('1')
+			... 
+			break;
+				.ext export ["API"]: is require
+{'void'};
+~
+  [-]: Async hostName resolve 
+/!\ Available... 
+	from nsock 
+		let try = m  a  g  i  c  k  a  .  l  u  a
+/!\ am not sure whether nsock should provide it or if it should simply provide an API to plug in an external system...
+	o Socks5 support
+		o code.exe is cp from ncat 
+$ ncat mv nbase
+▶ del && mod && mv Event List 
+	# struct .dat  
+	# ["USE"]: radix Tree.node 
+		Nmap Event IDs ➜ p  o  i  n  t  e  r  s.h 
+>>> <?php <solve>puts</solve> ["dat","single","robot","bool","tree"]: $valid BSD_HACK_MODE & 0x01 
+!--UTF 86 -- encoding 
+	Event type.js in ID.node MSB.node 
+		let "make" do in Order Traversal while <?php connect events first, then read, then write...?>
+{
+m  a  g  i  c  k  a  .  l  u  a
+echo "test"
+	date: oct 2013
+		bottle: neck 
+		break;
+	return value
+};
+o -re --work fs code.exe copy.dat Scatter/gatherI/O.py 
+➜ ["USE"]: task in "expression" : "profile and optimize the usual nmap nsock I/O pattern_"
+"quit"
