40cd8f6ad7c4e2b5e5b5724de7c48bdedee51af0
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-05-09 12:28:19 -0500

    ->قرص الفارس.py
    ^d^re^mod
    
    Signed-off-by: Z4 <30985576+TheProdigyLeague@users.noreply.github.com>

---
+++ todo/قرص الفارس.py
@@ -0,0 +1,100 @@
+NYSE::ABC|corp...GSoC_2011:\
+~
+>>> import GSoC_Vuln&Xploit.py 
+o https://secwiki.org/w/Nmap/Script_Ideas#Djalal_Harouni
+>>> import http://arch.nse/suggestions/comments&try 
+{
+include propose
+};
+o http://seclists.org/nmap-dev/2011/q2/967
+["START"]: /thread/ 
+/!\ nmap-dev @about ["USR"]: @fav -nmap & cmd.nse
+["CREATE"]: @special^page for IT in secwiki.org/site
+o This let "us" @create -> scan profile for Zenmap(TM)
+1) infra.nse
+o ["High priority"]:
+➜  Take a look at Dan's NSE XML output patch and try to commit it.
+/!\ http://seclists.org/nmap-dev/2011/q2/1230
+o NSE Version Numbering: http://seclists.org/nmap-dev/2010/q4/693
+["other","tasks"]:
+o propose 
+o duplicate scan 
+o int.proto.nse 192.0.0.0.1/10
+2) NSE Scripts:
+["Priorities", "Tasks"]:
+o NFS/RPC features:
+['+']: NFS READLINK 
+['-']: :root$@support~# nfs-ls -Show symbol.fs
+>>> support let nfs-ls show symbolic files.
+o Review NSE scripts and libs, and fixing bugs:
+['-']: Docs all new NFS procedures.
+["other","tasks"]:
+o NFS/RPC features:
+['+']: > 0auth support: Unix 0auth 
+['-']: NFSv4 supp 
+['+']: -recurse supp ➜ nfs-ls.nse \v
+/!\
+o ["create"]: new Rule "version_Rule" 
+➜ ["use"]: version.js category.py 
+# http://seclists.org/nmap-dev/2010/q3/551
+o script.NSE -d
+o ['+']: -> ctrl.NSE for 
+▶ long running script: 
+▶ 1.) option 
+>>> bool expression "filter" ('tcpdump') -mod script.nse nse.py args.js @@Result==$0
+~
+/!\ note: This is for big networks.
+▶ 2.) option
+>>> generic.nse nse.lua lua.py -read code.exe include ("expression","filter"): ["SELECT","USE"]: args let args/nse.py
+~
+# // however for the moment this is a simple idea and it needs further discussion on the nmap-dev.
+PS> privilege -exit for script.nse
+o nmap.sec
+o review/sec/nmap
+o FIX: bugs.
+['-']: src.nse \port \flag Version -S throw, "ERROR"
+▶ http://seclists.org/nmap-dev/2010/q2/576 
+note: support setting the source port
+▶  http://seclists.org/nmap-dev/2010/q3/331
+/!\ DONE:
+1) Nmap Scripting Engine Infrastructure:
+o Verbesserte untere Karte. Architektur des Hypertext-Übertragungsprotokolls.
+➜ http://seclists.org/nmap-dev/2011/q2/967
+>>> docker makefile script.nse script.js nse.py type.js -nmap uif info.net from 
+{
+<?php input | output?>
+# 使 | N  e  t  h  e  r | 地图脚本能够检索接口网络信息。
+};
+o LuaFileSystem /dir/ iterator ['1']: \port.py
+['1']: $http://keplerproject.github.com/luafilesystem/
+>>> ["create"]: class ** script.py ["use"]: int = 2 new.js new.py -Rule
+o 使用两个新脚本规则的新脚本类。
+['-']: ➜ Script Pre-Scan 
+&& Script Post-scan Rules: "prerule" && "postrule"**docs this __New_Phase ;; 
+break;['-']: ➜ Upd.py Upd.js @end_usr this 
+>>> dns-zone-transfer ["!"]: "prerule"*"portrule"
+o ^d other_part**Nmap.lib ["SHOW"]: NEW! "Scripts scanner phase."
+o FIX bug:
+["-"]: fix.nse exclude/dir 
+['!']: 修复错误并提交  ！#r18467
+o let pre-Rule.nse 
+# "portRule" ** "hostRule" .py .js | | ['+']:`(@(target(@Nmap)))_[o[^d[_scripting.xml["SHOW[NEW.py["scan"["phases"]]]]]]]
+2.) 下界地图脚本引擎。:break;
+▶ o smtp-vuln-cve2011-1764 script to check Exim DKIM Format String vulnerability (CVE-2011-1764).
+▶ o Updated and Improved ftp-vsftpd-backdoor to detect the vsFTPd backdoor (CVE-2011-2523).
+▶ o ftp-vuln-cve2010-4221.nse script to check the ProFTPD Telnet IAC stack overflow (CVE-2010-4221).
+▶ o smtp-vuln-cve2010-4344 script to check and exploit Exim SMTP Server: heap overflow (CVE-2010-4344) and privileges escalation `(CVE-2010-4345)
+▶ o SMTP library.
+▶ o Rewritten SMTP scripts to use the smtp library:
+['-']: smtp-commands
+['-']: smtp-open-relay
+['-']: smtp-enum-users
+▶ o smtp-vuln-cve2011-1720 script to check for CVE-2011-1720
+▶ o broadcast-avahi-dos script to check for CVE-2011-1002
+▶ o NFS/RPC features:
+['-']: New script: nfs-ls which combines nfs-dirlist and nfs-acls and try to emulates some features of the old "ls" unix tool. The script support | NFSv2 and NFSv3.
+['-']: Readapted the RPC and NFS library code with a new re-design with new high level functions.
+['-']: Added NFS procedures support:
+`NFSv2: LOOKUP~
+  `NFSv3: FSSTAT, FSINFO, READDIRPLUS, PATHCONF, ACCESS, LOOKUP~
+  "QUIT"
