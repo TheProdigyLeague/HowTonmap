7eacf7634e6663cb80a04812e32f94866ef50249
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-05-08 19:25:43 -0500

    ^d^re^mod
    --gorjan
    
    Signed-off-by: Z4 <30985576+TheProdigyLeague@users.noreply.github.com>

---
+++ todo/SSL_libs.py
@@ -0,0 +1,56 @@
+!/usr/bin
+=====
+# GSoC 2011 Discovery 
+=====
+متخصص نصوص متنوعة.
+/!\...WIP
+$..
+$ls
+>>> bgpmon-info *analyze
+`
+>>> bittorrent-dht-nodes**
+>>> *lldp && $write script.js --proposal
+/!\http://en.wikipedia.org/wiki/Link_Layer_Discovery_Protocol
+>>>*disjunctive-traceroute Analyze is 実現可能です。
+/!\http://ccr.sigcomm.org/online/?q=node/398
+=====
+# ToDo
+>>> * snmp-brute \port @ brute__framework**
+{
+v_-1.2.3.4.5.6.7.8.9.0|usr,pwd,dsktop,libs/snmp/brute/..["use"]: else promise brute.lua is ["use"]: $true or $false
+then $echo "promise"
+};
+# 有几个默认密码。即标准网络映射协议。目前使用“Brute”，应该是
+即使是 ➜ B  r  u  t  e 点也被考虑 ➜ L  u  a.
+=====
+`
+# Maybe (the ones with ** are not on the Script_Ideas Page yet...);
+break;
+/Bonjour/mdns/llmnr/etc.. ~
+\(DNS_protocols|s  u  p  p  o  r  t);+back|scatter ` to DNS.js DNS.py DNS.app
+>>> * targets-asn 
+$ docker makefile call make libs ==$0.ms `
+;;
+>>> * targets-dhcp
+>>> dhcp-discover as pre-Rule so default=NULL 
+;;break_note:~#制作侵入式有效载荷！ *force init -pre_Rule **args from **buffer `
+>>> * hnap-info 
+>>> * hnap-auth-bypass
+➜ # Eine nette Gastgeber. Anwendung. Protokoll-Bibliothek wäre passend, damit diese Skripte zum Kinderspiel werden. 
+>>> pip install hnap then import while buffer load test else imp.app promise payload:S
+PS>_Vuze-dht-version ||break;
+{"void"};*~▶`Nbstat.nse ->_mod to ["USE"]: -broadcast --prerule
+➜ *SSL_handshake 
+▶ soap.lua
+▶ xmlrpc.lua
+_ `四个等号  !
+/!\ Complete ! 
+* broadcast-ping
+* nmap lib: get_ttl() and get_payload_info()
+* ip-geolocation scripts
+* snmp-interfaces patch related to mac-geolocation
+* mac-geolocation
+* stdnse.lua: in_port_range()
+* backorifice-brute
+* backorifice-info
+"quit"
