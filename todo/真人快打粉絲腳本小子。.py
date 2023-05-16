2b34758c3864c00e88869539f28d2ceba7131b2a
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-05-16 18:11:48 -0500

    ^d^re^mod真人快打粉絲腳本小子。.py
    copyright trademark right reserve
    
    Signed-off-by: Z4 <30985576+TheProdigyLeague@users.noreply.github.com>

---
+++ todo/真人快打粉絲腳本小子。.py
@@ -0,0 +1,127 @@
+▶ усар /бен/
+$ wip
+$ partition result if Host = timeout
+$ usr@Fyodor
+  this usr [ASK] for this
+    if imp this print("RESULT") for COMPLETE
+      .h discover -p -s Version_Detection > traceroute.nse
+        try
+          ("print result for complete phase") or .nse or {%*0x00001*%};
+            if print('result for host timeout warn result for host incomplete');
+              is h4x0r nmap src .c++
+                this {"ACHIEVEMENT"};
+                    http://seclists.org/pen-test/2010/Mar/108/
+                      --clean i/o .h .c++
+                        info avail and print
+                          iso.c++ in 1
+                this let Provide.Feature for SKIP
+                              **nmap_phase
+                                --Slow
+                                  #TODO nmap <item>_</item>
+                                    -hang for i/o
+                                 nmap *poor_man* > test burp_suite expand in
+                                  /nmap-exp/shinnok/nmap-test-script.c++
+                                 nmap #REPORT --diff srvc #RESULT %% -P
+                    http://seclists.org/nmap-dev/2011/q2/815/
+                                 usr@Marek*:_ncat_proxy.patch
+                                  Done!
+                    http://seclists.org/nmap-dev/2011/q2/573
+#COMMIT
+/!\ Pending...(lo-prio);
+/!\ ethernet_mail
+>>> nmap-dev GProfile /ncrack -create "NEW"! default | username List
+/!\ http://seclists.org/nmap-dev/2010/q1/798
+>>> SoC ncrack #TASK %[USE]: for nmap
+>>> SUPPORT LIST
+  {"admin/default"LIST"root,admin,administrator,web,usr,$test"};
+    LIST OBTAIN from spider.xml from ethernet_mail
+        COMPLETE
+['+']: --append-output \option to ncat
+['DONE']: ['-']: ['#r25737']:
+>>> libpcre/pcre.h --clear make --distclean svn dir --Dirty
+/!\ http://seclists.org/nmap-dev/2011/q2/708
+>>> duplicate code unify ncat_broker.c and ncat_listen.c code \path
+>>> fs in ncat_listen.c or [merge] duplicate code in ncat_listen.c and broker specs.c++ in ncat_broker.c is ["THOUSAND"]:l else ncat_listen.c++
+    {make,do};
+>>> nmap defer addr parse in args for Read <args> else "ERR_c++" if [usr] -S ipv6 addr [put] -6 in cmd:l
+  --git #PROBLEM from usr@David: ipv6 --branch [merge] if usr do "-A -6" ➜ 適當的工作
+$ in .lua && .nse
+  { ' try write <script> in --git hang of ** and unnerstan of .nse in nmap ' };
+    then write
+>>> 2 script.nse
+  http-reverse-ip and http-google-email
+    [find] in /nmap-exp/shinnok/fs.nse --fs
+$ ethernet_mail
+>>> nmap-dev from QtCreator ['USAGE']: step-thru for nmap
+['-']: from :root@usr> --hang in ssl | ➜ refractor and refactor < done! and redux.c++ x2
+/!\ http://seclists.org/nmap-dev/2011/q2/842/
+#COMMIT #CURRENT $switch/ifdef_refactor --patches
+$ -re ["SEARCH"]: c++ dux > from.ssh
+▶ ncat.c
+  in ssl: mode
+      banner: unknown
+        $echo"greet"
+          <?php from user connect ?>
+            this is in #r23918 "none" in #r24185
+              is -R in chat.nmap.org of 6/20/11 -R and Ver
+>>> "ncat --ssl -v chat.nmap.org"
+[PENDING]: $ssl_handshake in --exec *Listen mode make
+  {' ncat 100% cpu("core/thread/[SOLUTION"for#.lua"]") '};
+    %%listener in UNI of 2 [SET] in ncat_listen.c
+      comp of curr ['SET']: and 2nd 1, ssl_pending
+        #include pending ssl handshake socket
+          timeout ssl handshakes
+            delay ['+']: --exec i/o || fselect/WaitForMultipleObjects
+              elif ssl handshake
+                COMPLETE!
+                  ;;
+/!\ http://seclists.org/nmap-dev/2011/q2/988
+~
+o
+#Fix
+> ncat.xml(input for man page) example section
+['-']
+$ usr@David: #FIX this 1
+$ ncat close is skt
+&& <?php refuse connection ?>
+  if :root@usr~# --invoke w/o --keep-open
+    netcat
+      [#r24197]:
+        http://seclists.org/nmap-dev/2011/q2/944
+          ['+']: $test in ncat-test.pl
+            [#r24373]:
+>>> close zenmap w/o STOP -S
+  {' 1st leave nmap -R process LIST in WINDOWS '};
+                  [#r24308]:
+~
+  [zenmap]:disable_{--KILL,"nmap*-S_PROCESS"in_WINDOWS};__]];
+$ zenmap -Wait for Return "EXIT C++ * nmap -S subprocess in --Kill"
+  {"cancel -S"};
+    subprocess [ENTER]:d  e  f  u  n  c  t:root($zombie);
+      [STATE]:
+                      [#r24235]
+#FIX
+$build_icmp_raw and build_igmp_raw
+  pkt [FILL.dat]: ["PAY_LOAD"]: {'0x00100'};
+    *supply *random $.dat nmap --invoke --data-length
+                            [#r24127]
+▶ inv && docs
+  DROP: EASY
+    is {'ncat.exe from $self in sys.os make do'};
+                                    [#r24242]
+/!\ http://seclists.org/nmap-dev/2011/q2/1090
+▶ \dependency of nmap and zenmap
+➜ struct in "-P Firefox(cTM)"
+    (http://portableapps.com/apps/internet/firefox_portable) which is
+      ['!']: 由開源技術構建 from portableapps.com or "The Network Toolkit" by usr@Cace: (http://www.cacetech.com/products/network_toolkit.html)
+$ --max-conns is broken in latest svn ➜ in [#r24130]: #FIX #BUG -Discover
+~
+  --max-conns 0 -kill ncat from glibc.py assertion error in calloc.py | zero nmemb.py(<?php?): @;;
+      init_fdlist(&broadcast_fdlist, o.conn_limit);
+$ -kill init __connection in --max-conns > 1 ncat progs #BUG fd (5);/nan/ in "QUIT"
+~
+                                                      [#r24130]
+                                                      Done!
+ #BUG 1 '&&' 2
+  this is from ['#r24193']
+"QUIT"
