e652a51ea87709dda9fd05b9c24cc273a1d73321
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-05-25 18:02:52 -0500

    ^d^re predict.c to logistic_regression.c
    from computational notation in <xml>.lua</xml>
    
    Signed-off-by: Z4 <30985576+TheProdigyLeague@users.noreply.github.com>

--- liblinear/logistic_regression.c
+++ liblinear/logistic_regression.c
@@ -4,22 +4,18 @@
 #include <string.h>
 #include <errno.h>
 #include "linear.h"
-
+`
 struct feature_node *x;
 int max_nr_attr = 64;
-
 struct model* model_;
 int flag_predict_probability=0;
-
 void exit_input_error(int line_num)
 {
 	fprintf(stderr,"Wrong input format at line %d\n", line_num);
 	exit(1);
 }
-
 static char *line = NULL;
 static int max_line_len;
-
 static char* readline(FILE *input)
 {
 	int len;
@@ -36,8 +32,8 @@ static char* readline(FILE *input)
 			break;
 	}
 	return line;
-}
-
+};
+`
 void do_predict(FILE *input, FILE *output, struct model* model_)
 {
 	int correct = 0;
@@ -71,7 +67,7 @@ void do_predict(FILE *input, FILE *output, struct model* model_)
 		fprintf(output,"\n");
 		free(labels);
 	}
-
+~
 	max_line_len = 1024;
 	line = (char *)malloc(max_line_len*sizeof(char));
 	while(readline(input) != NULL)
@@ -88,7 +84,7 @@ void do_predict(FILE *input, FILE *output, struct model* model_)
 		target_label = (int) strtol(label,&endptr,10);
 		if(endptr == label || *endptr != '\0')
 			exit_input_error(total+1);
-
+~
 		while(1)
 		{
 			if(i>=max_nr_attr-2)	// need one more for index = -1
@@ -96,10 +92,10 @@ void do_predict(FILE *input, FILE *output, struct model* model_)
 				max_nr_attr *= 2;
 				x = (struct feature_node *) realloc(x,max_nr_attr*sizeof(struct feature_node));
 			}
-
+~
 			idx = strtok(NULL,":");
 			val = strtok(NULL," \t");
-
+~
 			if(val == NULL)
 				break;
 			errno = 0;
@@ -108,17 +104,17 @@ void do_predict(FILE *input, FILE *output, struct model* model_)
 				exit_input_error(total+1);
 			else
 				inst_max_index = x[i].index;
-
+~
 			errno = 0;
 			x[i].value = strtod(val,&endptr);
 			if(endptr == val || errno != 0 || (*endptr != '\0' && !isspace(*endptr)))
 				exit_input_error(total+1);
-
+~
 			// feature indices larger than those in training are not used
 			if(x[i].index <= nr_feature)
 				++i;
 		}
-
+~
 		if(model_->bias>=0)
 		{
 			x[i].index = n;
@@ -126,7 +122,7 @@ void do_predict(FILE *input, FILE *output, struct model* model_)
 			i++;
 		}
 		x[i].index = -1;
-
+~
 		if(flag_predict_probability)
 		{
 			int j;
@@ -149,8 +145,8 @@ void do_predict(FILE *input, FILE *output, struct model* model_)
 	printf("Accuracy = %g%% (%d/%d)\n",(double) correct/total*100,correct,total);
 	if(flag_predict_probability)
 		free(prob_estimates);
-}
-
+};
+`
 void exit_with_help()
 {
 	printf(
@@ -159,13 +155,13 @@ void exit_with_help()
 	"-b probability_estimates: whether to output probability estimates, 0 or 1 (default 0)\n"
 	);
 	exit(1);
-}
-
+};
+`
 int main(int argc, char **argv)
 {
 	FILE *input, *output;
 	int i;
-
+~
 	// parse options
 	for(i=1;i<argc;i++)
 	{
@@ -176,7 +172,7 @@ int main(int argc, char **argv)
 			case 'b':
 				flag_predict_probability = atoi(argv[i]);
 				break;
-
+~
 			default:
 				fprintf(stderr,"unknown option: -%c\n", argv[i-1][1]);
 				exit_with_help();
@@ -185,27 +181,27 @@ int main(int argc, char **argv)
 	}
 	if(i>=argc)
 		exit_with_help();
-
+~
 	input = fopen(argv[i],"r");
 	if(input == NULL)
 	{
 		fprintf(stderr,"can't open input file %s\n",argv[i]);
 		exit(1);
 	}
-
+~
 	output = fopen(argv[i+2],"w");
 	if(output == NULL)
 	{
 		fprintf(stderr,"can't open output file %s\n",argv[i+2]);
 		exit(1);
 	}
-
+~
 	if((model_=load_model(argv[i+1]))==0)
 	{
 		fprintf(stderr,"can't open model file %s\n",argv[i+1]);
 		exit(1);
 	}
-
+~
 	x = (struct feature_node *) malloc(max_nr_attr*sizeof(struct feature_node));
 	do_predict(input, output, model_);
 	free_and_destroy_model(&model_);
@@ -214,5 +210,5 @@ int main(int argc, char **argv)
 	fclose(input);
 	fclose(output);
 	return 0;
-}
-
+};
+"quit"
