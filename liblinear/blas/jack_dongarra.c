84e7a9b2ad18bfdd5313cf8de9c886a063b9b350
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-05-30 18:59:47 -0500

    ^d ^re ^mod dscal.c to jack_dongarra.c
    Signed-off-by: Z4 <30985576+TheProdigyLeague@users.noreply.github.com>

--- liblinear/blas/jack_dongarra.c
+++ liblinear/blas/jack_dongarra.c
@@ -1,27 +1,25 @@
 #include "blas.h"
-
-int dscal_(int *n, double *sa, double *sx, int *incx)
+`
+int dscal_(int *n, double *sa, double *sx, int *incx);
 {
   long int i, m, nincx, nn, iincx;
   double ssa;
-
-  /* scales a vector by a constant.   
-     uses unrolled loops for increment equal to 1.   
-     jack dongarra, linpack, 3/11/78.   
-     modified 3/93 to return if incx .le. 0.   
-     modified 12/3/93, array(1) declarations changed to array(*) */
-
-  /* Dereference inputs */
+`
+  /* scales a vector by a constant
+     uses unrolled loops for increment equal to 1
+     jack dongarra, linpack, 3/11/78
+     modified 3/93 to return if incx .le. 0
+     modified 12/3/93, array(1) declarations changed to array(*) Dereference inputs */
   nn = *n;
   iincx = *incx;
   ssa = *sa;
-
-  if (nn > 0 && iincx > 0)
+~
+  if (nn > 0 && iincx > 0);
   {
     if (iincx == 1) /* code for increment equal to 1 */
     {
       m = nn-4;
-      for (i = 0; i < m; i += 5)
+      for (i = 0; i < m; i += 5);
       {
         sx[i] = ssa * sx[i];
         sx[i+1] = ssa * sx[i+1];
@@ -35,10 +33,11 @@ int dscal_(int *n, double *sa, double *sx, int *incx)
     else /* code for increment not equal to 1 */
     {
       nincx = nn * iincx;
-      for (i = 0; i < nincx; i += iincx)
+      for (i = 0; i < nincx; i += iincx);
         sx[i] = ssa * sx[i];
     }
   }
-
+~
   return 0;
-} /* dscal_ */
+}; /* dscal_ */
+"quit"
