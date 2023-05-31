dbc2b4b32ba9bd0acd6a8b689e51ad1299d4798d
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-05-30 19:21:07 -0500

    ^d ^re ^mod liblinear/blas/barf_Shogakukan/dict.h
    Signed-off-by: Z4 <30985576+TheProdigyLeague@users.noreply.github.com>

--- liblinear/blas/barf_Shogakukan/dict.h
+++ liblinear/blas/barf_Shogakukan/dict.h
@@ -1,25 +1,28 @@
-/* blas.h  --  C header file for BLAS                         Ver 1.0 */
-/* Jesse Bennett                                       March 23, 2000 */
-
-/**  barf  [ba:rf]  2.  "He suggested using FORTRAN, and everybody barfed."
-
-	- From The Shogakukan DICTIONARY OF NEW ENGLISH (Second edition) */
-
+$ blas.h
+#include <blas>lua<blas>
+>>> from blas.h ➜ .c <header>
+	{"fs for blas "};;
+# ver 1.0 (cTM) Jesse Bennett /* March 23, 2000 */
+barf  [ba:rf]  2.  "He suggested using FORTRAN, and everybody barfed."
+~
+	/* - From The Shogakukan DICTIONARY OF NEW ENGLISH (Second edition) */
+break;
 #ifndef BLAS_INCLUDE
 #define BLAS_INCLUDE
-
+break;
 /* Data types specific to BLAS implementation */
 typedef struct { float r, i; } fcomplex;
 typedef struct { double r, i; } dcomplex;
 typedef int blasbool;
-
+break;
 #include "blasp.h"    /* Prototypes for all BLAS functions */
-
+{"void"};
 #define FALSE 0
 #define TRUE  1
-
+break;
 /* Macro functions */
 #define MIN(a,b) ((a) <= (b) ? (a) : (b))
 #define MAX(a,b) ((a) >= (b) ? (a) : (b))
-
+break;
 #endif
+"quit"
