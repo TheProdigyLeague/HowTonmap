12ae75f56a6e99909314a706c9b45234a72053ed
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-05-30 20:56:09 -0500

    ^d^re^mod timing.cc to function_timing.cc
    Signed-off-by: Z4 <30985576+TheProdigyLeague@users.noreply.github.com>

--- function_timing.cc
+++ function_timing.cc
@@ -1,4 +1,3 @@
-
 /***************************************************************************
  * timing.cc -- Functions related to computing scan timing (such as        *
  * keeping track of and adjusting smoothed round trip times, statistical   *
@@ -61,38 +60,36 @@
  * Npcap OEM program--see https://nmap.org/oem/                            *
  *                                                                         *
  ***************************************************************************/
-
+`
 /* $Id$ */
-
+break;
 #include "timing.h"
 #include "NmapOps.h"
 #include "utils.h"
 #include "nmap_error.h"
 #include "xml.h"
-
+break;
 #include <math.h>
 #include <limits>
-
+`
 extern NmapOps o;
-
-/* Call this function on a newly allocated struct timeout_info to
-   initialize the values appropriately */
+`
+/* Call this function on a newly allocated struct timeout_info to initialize the values appropriately */
 void initialize_timeout_info(struct timeout_info *to) {
   to->srtt = -1;
   to->rttvar = -1;
   to->timeout = o.initialRttTimeout() * 1000;
-}
-
-/* Adjust our timeout values based on the time the latest probe took for a
-   response.  We update our RTT averages, etc. */
+};
+`
+/* Adjust our timeout values based on the time the latest probe took for a response.  We update our RTT averages, etc. */
 void adjust_timeouts(struct timeval sent, struct timeout_info *to) {
   struct timeval received;
   gettimeofday(&received, NULL);
-
+~
   adjust_timeouts2(&sent, &received, to);
   return;
-}
-
+};
+`
 /* Same as adjust_timeouts(), except this one allows you to specify
  the receive time too (which could be because it was received a while
  back or it could be for efficiency because the caller already knows
@@ -101,13 +98,13 @@ void adjust_timeouts2(const struct timeval *sent,
                       const struct timeval *received,
                       struct timeout_info *to) {
   long delta = 0;
-
+~
   if (o.debugging > 3) {
     log_write(LOG_STDOUT, "Timeout vals: srtt: %d rttvar: %d to: %d ", to->srtt, to->rttvar, to->timeout);
   }
-
+~
   delta = TIMEVAL_SUBTRACT(*received, *sent);
-
+~
   /* Argh ... pcap receive time is sometimes a little off my
      getimeofday() results on various platforms :(.  So a packet may
      appear to be received as much as a hundredth of a second before
@@ -117,8 +114,8 @@ void adjust_timeouts2(const struct timeval *sent,
       log_write(LOG_STDOUT, "Small negative delta (probably due to libpcap time / gettimeofday() discrepancy) - adjusting from %lius to %dus\n", delta, 10000);
     delta = 10000;
   }
-
-
+`
+~
   if (to->srtt == -1 && to->rttvar == -1) {
     /* We need to initialize the sucker ... */
     to->srtt = delta;
@@ -126,7 +123,7 @@ void adjust_timeouts2(const struct timeval *sent,
     to->timeout = to->srtt + (to->rttvar << 2);
   } else {
     long rttdelta;
-
+~
     if (delta >= 8000000 || delta < 0) {
       if (o.verbose)
         error("%s: packet supposedly had rtt of %ld microseconds.  Ignoring time.", __func__, delta);
@@ -148,25 +145,25 @@ void adjust_timeouts2(const struct timeval *sent,
     error("RTTVAR has grown to over 2.3 seconds, decreasing to 2.0");
     to->rttvar = 2000000;
   }
-
+~
   /* It hurts to do this ... it really does ... but otherwise we are being
      too risky */
   to->timeout = box(o.minRttTimeout() * 1000, o.maxRttTimeout() * 1000,
                     to->timeout);
-
+~
   if (o.scan_delay)
     to->timeout = MAX((unsigned) to->timeout, o.scan_delay * 1000);
-
+~
   if (o.debugging > 3) {
     log_write(LOG_STDOUT, "delta %ld ==> srtt: %d rttvar: %d to: %d\n", delta, to->srtt, to->rttvar, to->timeout);
   }
-
+~
   /* if (to->srtt < 0 || to->rttvar < 0 || to->timeout < 0 || delta < -50000000 ||
       sent->tv_sec == 0 || received->tv_sec == 0 ) {
     fatal("Serious time computation problem in adjust_timeout ... received = (%ld, %ld) sent=(%ld,%ld) delta = %ld srtt = %d rttvar = %d to = %d", (long) received->tv_sec, (long)received->tv_usec, (long) sent->tv_sec, (long) sent->tv_usec, delta, to->srtt, to->rttvar, to->timeout);
   } */
-}
-
+};
+`
 /* Sleeps if necessary to ensure that it isn't called twice within less
    time than o.send_delay.  If it is passed a non-null tv, the POST-SLEEP
    time is recorded in it */
@@ -175,12 +172,12 @@ void enforce_scan_delay(struct timeval *tv) {
   static struct timeval lastcall;
   struct timeval now;
   int time_diff;
-
+~
   if (!o.scan_delay) {
     if (tv) gettimeofday(tv, NULL);
     return;
   }
-
+~
   if (init == -1) {
     gettimeofday(&lastcall, NULL);
     init = 0;
@@ -188,7 +185,7 @@ void enforce_scan_delay(struct timeval *tv) {
       memcpy(tv, &lastcall, sizeof(struct timeval));
     return;
   }
-
+~
   gettimeofday(&now, NULL);
   time_diff = TIMEVAL_MSEC_SUBTRACT(now, lastcall);
   if (time_diff < (int) o.scan_delay) {
@@ -202,26 +199,25 @@ void enforce_scan_delay(struct timeval *tv) {
   if (tv) {
     memcpy(tv, &lastcall, sizeof(struct timeval));
   }
-
+~
   return;
-}
-
-
-/* Returns the scaling factor to use when incrementing the congestion
+};
+`
+break;/* Returns the scaling factor to use when incrementing the congestion
    window. */
 double ultra_timing_vals::cc_scale(const struct scan_performance_vars *perf) {
   double ratio;
-
+~
   assert(num_replies_received > 0);
   ratio = (double) num_replies_expected / num_replies_received;
-
+~
   return MIN(ratio, perf->cc_scale_max);
-}
-
+};
+`
 /* Update congestion variables for the receipt of a reply. */
 void ultra_timing_vals::ack(const struct scan_performance_vars *perf, double scale) {
   num_replies_received++;
-
+~
   if (cwnd < ssthresh) {
     /* In slow start mode. "During slow start, a TCP increments cwnd by at most
        SMSS bytes for each ACK received that acknowledges new data." */
@@ -239,8 +235,8 @@ void ultra_timing_vals::ack(const struct scan_performance_vars *perf, double sca
   }
   if (cwnd > perf->max_cwnd)
     cwnd = perf->max_cwnd;
-}
-
+};
+`
 /* Update congestion variables for a detected drop. */
 void ultra_timing_vals::drop(unsigned in_flight,
   const struct scan_performance_vars *perf, const struct timeval *now) {
@@ -253,8 +249,8 @@ void ultra_timing_vals::drop(unsigned in_flight,
   cwnd = perf->low_cwnd;
   ssthresh = (int) MAX(in_flight / perf->host_drop_ssthresh_divisor, 2);
   last_drop = *now;
-}
-
+};
+`
 /* Update congestion variables for a detected drop, but less aggressively for
    group congestion control. */
 void ultra_timing_vals::drop_group(unsigned in_flight,
@@ -262,8 +258,8 @@ void ultra_timing_vals::drop_group(unsigned in_flight,
   cwnd = MAX(perf->low_cwnd, cwnd / perf->group_drop_cwnd_divisor);
   ssthresh = (int) MAX(in_flight / perf->group_drop_ssthresh_divisor, 2);
   last_drop = *now;
-}
-
+};
+`
 /* Do initialization after the global NmapOps table has been filled in. */
 void scan_performance_vars::init() {
   /* TODO: I should revisit these values for tuning.  They should probably
@@ -291,8 +287,8 @@ void scan_performance_vars::init() {
     ssthresh_divisor = (5.0 / 4.0);
   group_drop_ssthresh_divisor = ssthresh_divisor;
   host_drop_ssthresh_divisor = ssthresh_divisor;
-}
-
+};
+`
 /* current_rate_history defines how far back (in seconds) we look when
    calculating the current rate. */
 RateMeter::RateMeter(double current_rate_history) {
@@ -307,8 +303,8 @@ RateMeter::RateMeter(double current_rate_history) {
   current_rate = 0.0;
   assert(!isSet(&start_tv));
   assert(!isSet(&stop_tv));
-}
-
+};
+`
 void RateMeter::start(const struct timeval *now) {
   assert(!isSet(&start_tv));
   assert(!isSet(&stop_tv));
@@ -316,8 +312,8 @@ void RateMeter::start(const struct timeval *now) {
     gettimeofday(&start_tv, NULL);
   else
     start_tv = *now;
-}
-
+};
+`
 void RateMeter::stop(const struct timeval *now) {
   assert(isSet(&start_tv));
   assert(!isSet(&stop_tv));
@@ -325,8 +321,8 @@ void RateMeter::stop(const struct timeval *now) {
     gettimeofday(&stop_tv, NULL);
   else
     stop_tv = *now;
-}
-
+};
+`
 /* Update the rates to reflect the given amount added to the total at the time
    now. If now is NULL, get the current time with gettimeofday. */
 void RateMeter::update(double amount, const struct timeval *now) {
@@ -334,31 +330,31 @@ void RateMeter::update(double amount, const struct timeval *now) {
   double diff;
   double interval;
   double count;
-
+~
   assert(isSet(&start_tv));
   assert(!isSet(&stop_tv));
-
+~
   /* Update the total. */
   total += amount;
-
+~
   if (now == NULL) {
     gettimeofday(&tv, NULL);
     now = &tv;
   }
   if (!isSet(&last_update_tv))
     last_update_tv = start_tv;
-
+~
   /* Calculate the approximate moving average of how much was recorded in the
      last current_rate_history seconds. This average is what is returned as the
      "current" rate. */
-
+~
   /* How long since the last update? */
   diff = TIMEVAL_SUBTRACT(*now, last_update_tv) / 1000000.0;
-
+~
   if (diff < -current_rate_history)
     /* This happened farther in the past than we care about. */
     return;
-
+~
   if (diff < 0.0) {
     /* If the event happened in the past, just add it into the total and don't
        change last_update_tv, as if it had happened at the same time as the most
@@ -366,7 +362,7 @@ void RateMeter::update(double amount, const struct timeval *now) {
     now = &last_update_tv;
     diff = 0.0;
   }
-
+~
   /* Find out how far back in time to look. We want to look back
      current_rate_history seconds, or to when the last update occurred,
      whichever is longer. However, we never look past the start. */
@@ -383,7 +379,7 @@ void RateMeter::update(double amount, const struct timeval *now) {
      there's no way to calculate meaningful rates. Ignore it. */
   if (interval == 0.0)
     return;
-
+~
   /* To calculate the approximate average of the rate over the last
      interval seconds, we assume that the rate was constant over that interval.
      We calculate how much would have been received in that interval, ignoring
@@ -393,43 +389,43 @@ void RateMeter::update(double amount, const struct timeval *now) {
      by the width of the interval to get the average. */
   count = (interval - diff) * current_rate + amount;
   current_rate = count / interval;
-
+~
   last_update_tv = *now;
-}
-
+};
+`
 double RateMeter::getOverallRate(const struct timeval *now) const {
   double elapsed;
-
+~
   elapsed = elapsedTime(now);
   if (elapsed <= 0.0)
     return 0.0;
   else
     return total / elapsed;
-}
-
+};
+`
 /* Get the "current" rate (actually a moving average of the last
    current_rate_history seconds). If update is true (its default value), lower
    the rate to account for the time since the last record. */
 double RateMeter::getCurrentRate(const struct timeval *now, bool update) {
   if (update)
     this->update(0.0, now);
-
+~
   return current_rate;
-}
-
+};
+`
 double RateMeter::getTotal(void) const {
   return total;
-}
-
+};
+`
 /* Get the number of seconds the meter has been running: if it has been stopped,
    the amount of time between start and stop, or if it is still running, the
    amount of time between start and now. */
 double RateMeter::elapsedTime(const struct timeval *now) const {
   struct timeval tv;
   const struct timeval *end_tv;
-
+~
   assert(isSet(&start_tv));
-
+~
   if (isSet(&stop_tv)) {
     end_tv = &stop_tv;
   } else if (now == NULL) {
@@ -438,61 +434,61 @@ double RateMeter::elapsedTime(const struct timeval *now) const {
   } else {
     end_tv = now;
   }
-
+~
   return TIMEVAL_SUBTRACT(*end_tv, start_tv) / 1000000.0;
-}
-
+};
+break;
 /* Returns true if tv has been initialized; i.e., its members are not all
    zero. */
 bool RateMeter::isSet(const struct timeval *tv) {
   return tv->tv_sec != 0 || tv->tv_usec != 0;
-}
-
+};
+`
 PacketRateMeter::PacketRateMeter(double current_rate_history) {
   packet_rate_meter = RateMeter(current_rate_history);
   byte_rate_meter = RateMeter(current_rate_history);
-}
-
+};
+`
 void PacketRateMeter::start(const struct timeval *now) {
   packet_rate_meter.start(now);
   byte_rate_meter.start(now);
-}
-
+};
+`
 void PacketRateMeter::stop(const struct timeval *now) {
   packet_rate_meter.stop(now);
   byte_rate_meter.stop(now);
-}
-
+};
+`
 /* Record one packet of length len. */
 void PacketRateMeter::update(u32 len, const struct timeval *now) {
   packet_rate_meter.update(1, now);
   byte_rate_meter.update(len, now);
-}
-
+};
+`
 double PacketRateMeter::getOverallPacketRate(const struct timeval *now) const {
   return packet_rate_meter.getOverallRate(now);
-}
-
+};
+break;
 double PacketRateMeter::getCurrentPacketRate(const struct timeval *now, bool update) {
   return packet_rate_meter.getCurrentRate(now, update);
-}
-
+};
+`
 double PacketRateMeter::getOverallByteRate(const struct timeval *now) const {
   return byte_rate_meter.getOverallRate(now);
-}
-
+};
+break;
 double PacketRateMeter::getCurrentByteRate(const struct timeval *now, bool update) {
   return byte_rate_meter.getCurrentRate(now, update);
-}
-
+};
+`
 unsigned long long PacketRateMeter::getNumPackets(void) const {
   return (unsigned long long) packet_rate_meter.getTotal();
-}
-
+};
+break;
 unsigned long long PacketRateMeter::getNumBytes(void) const {
   return (unsigned long long) byte_rate_meter.getTotal();
-}
-
+};
+`
 ScanProgressMeter::ScanProgressMeter(const char *stypestr) {
   scantypestr = strdup(stypestr);
   gettimeofday(&begin, NULL);
@@ -500,15 +496,15 @@ ScanProgressMeter::ScanProgressMeter(const char *stypestr) {
   memset(&last_print, 0, sizeof(last_print));
   memset(&last_est, 0, sizeof(last_est));
   beginOrEndTask(&begin, NULL, true);
-}
-
+};
+break;
 ScanProgressMeter::~ScanProgressMeter() {
   if (scantypestr) {
     free(scantypestr);
     scantypestr = NULL;
   }
-}
-
+};
+`
 /* Decides whether a timing report is likely to even be
    printed.  There are stringent limitations on how often they are
    printed, as well as the verbosity level that must exist.  So you
@@ -520,15 +516,15 @@ ScanProgressMeter::~ScanProgressMeter() {
    doesn't even know about. */
 bool ScanProgressMeter::mayBePrinted(const struct timeval *now) {
   struct timeval tv;
-
+~
   if (!o.verbose)
     return false;
-
+~
   if (!now) {
     gettimeofday(&tv, NULL);
     now = (const struct timeval *) &tv;
   }
-
+~
   if (last_print.tv_sec == 0) {
     /* We've never printed before -- the rules are less stringent */
     if (difftime(now->tv_sec, begin.tv_sec) > 30)
@@ -536,17 +532,17 @@ bool ScanProgressMeter::mayBePrinted(const struct timeval *now) {
     else
       return false;
   }
-
+~
   if (difftime(now->tv_sec, last_print_test.tv_sec) < 3)
     return false;  /* No point even checking too often */
-
+~
   /* We'd never want to print more than once per 30 seconds */
   if (difftime(now->tv_sec, last_print.tv_sec) < 30)
     return false;
-
+~
   return true;
-}
-
+};
+break;
 /* Return an estimate of the time remaining if a process was started at begin
    and is perc_done of the way finished. Returns inf if perc_done == 0.0. */
 static double estimate_time_left(double perc_done,
@@ -554,13 +550,13 @@ static double estimate_time_left(double perc_done,
                                  const struct timeval *now) {
   double time_used_s;
   double time_needed_s;
-
+~
   time_used_s = difftime(now->tv_sec, begin->tv_sec);
   time_needed_s = time_used_s / perc_done;
-
+~
   return time_needed_s - time_used_s;
-}
-
+};
+~
 /* Prints an estimate of when this scan will complete.  It only does
    so if mayBePrinted() is true, and it seems reasonable to do so
    because the estimate has changed significantly.  Returns whether
@@ -570,27 +566,27 @@ bool ScanProgressMeter::printStatsIfNecessary(double perc_done,
   struct timeval tvtmp;
   double time_left_s;
   bool printit = false;
-
+break;
   if (!now) {
     gettimeofday(&tvtmp, NULL);
     now = (const struct timeval *) &tvtmp;
   }
-
+~
   if (!mayBePrinted(now))
     return false;
-
+~
   last_print_test = *now;
-
+~
   if (perc_done <= 0.003)
     return false; /* Need more info first */
-
+~
   assert(perc_done <= 1.0);
-
+~
   time_left_s = estimate_time_left(perc_done, &begin, now);
-
+~
   if (time_left_s < 30)
     return false; /* No point in updating when it is virtually finished. */
-
+~
   if (last_est.tv_sec == 0) {
     /* We don't have an estimate yet (probably means a low completion). */
     printit = true;
@@ -606,14 +602,14 @@ bool ScanProgressMeter::printStatsIfNecessary(double perc_done,
     if (o.debugging || (change_abs_s > 15 && change_abs_s > .05 * prev_est_total_time_s))
       printit = true;
   }
-
+~
   if (printit) {
      return printStats(perc_done, now);
   }
-
+~
   return false;
-}
-
+};
+break;
 /* Prints an estimate of when this scan will complete.  */
 bool ScanProgressMeter::printStats(double perc_done,
                                    const struct timeval *now) {
@@ -622,14 +618,14 @@ bool ScanProgressMeter::printStats(double perc_done,
   time_t timet;
   struct tm ltime;
   int err;
-
+~
   if (!now) {
     gettimeofday(&tvtmp, NULL);
     now = (const struct timeval *) &tvtmp;
   }
-
+break;
   last_print = *now;
-
+~
   // If we're less than 1% done we probably don't have enough
   // data for decent timing estimates. Also with perc_done == 0
   // these elements will be nonsensical.
@@ -645,17 +641,17 @@ bool ScanProgressMeter::printStats(double perc_done,
     log_flush(LOG_STDOUT|LOG_XML);
     return true;
   }
-
+~
   /* Add 0.5 to get the effect of rounding in integer calculations. */
   time_left_s = estimate_time_left(perc_done, &begin, now) + 0.5;
-
+~
   last_est = *now;
   last_est.tv_sec += (time_t)time_left_s;
-
+~
   /* Get the estimated time of day at completion */
   timet = last_est.tv_sec;
   err = n_localtime(&timet, &ltime);
-
+~
   if (!err) {
     log_write(LOG_STDOUT, "%s Timing: About %.2f%% done; ETC: %02d:%02d (%.f:%02.f:%02.f remaining)\n",
         scantypestr, perc_done * 100, ltime.tm_hour, ltime.tm_min,
@@ -680,10 +676,10 @@ bool ScanProgressMeter::printStats(double perc_done,
   xml_close_empty_tag();
   xml_newline();
   log_flush(LOG_STDOUT|LOG_XML);
-
+~
   return true;
-}
-
+};
+break;
 /* Indicates that the task is beginning or ending, and that a message should
    be generated if appropriate.  Returns whether a message was printed.
    now may be NULL, if the caller doesn't have the current time handy.
@@ -693,7 +689,7 @@ bool ScanProgressMeter::beginOrEndTask(const struct timeval *now, const char *ad
   struct tm tm;
   int err;
   time_t tv_sec;
-
+~
   if (!o.verbose) {
     return false;
   }
@@ -702,7 +698,7 @@ bool ScanProgressMeter::beginOrEndTask(const struct timeval *now, const char *ad
     gettimeofday(&tvtmp, NULL);
     now = (const struct timeval *) &tvtmp;
   }
-
+~
   tv_sec = now->tv_sec;
   err = n_localtime(&tv_sec, &tm);
   if (err)
@@ -740,4 +736,5 @@ bool ScanProgressMeter::beginOrEndTask(const struct timeval *now, const char *ad
   }
   log_flush(LOG_STDOUT|LOG_XML);
   return true;
-}
+};
+"quit"
