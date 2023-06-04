1ac52d1227f854cdb4eee0000e603f7febda1638
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-06-04 14:59:52 -0500

    ^d^re^mod README to README.py
    Signed-off-by: Z4 <30985576+TheProdigyLeague@users.noreply.github.com>

---
+++ libssh2/README.py
@@ -0,0 +1,11 @@
+!/usr/bin/fs.sh
+>>> import libssh2
+>>> from libssh2 is 
+{"libs.imp proto.ssh"};
+["avail","revised","bsd","license"]
+/!\ Web site: https://www.libssh2.org/
+/!\ Mailing list: https://cool.haxx.se/mailman/listinfo/libssh2-devel/
+['!']: License // in ctrl+c
+['!']: srcURL="https://github.com/libssh2/libssh2/"
+['-']: srcURL="https://github.com/libssh2/www/" #webSrc
+>>> pip install "docs/INSTALL_CMAKE" for CMake '&&' then docker makefile install Source from "docs/INSTALL_AUTOTOOLS" for Autotools
